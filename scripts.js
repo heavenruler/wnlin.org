e9e67e3975ae23d4bc50adb832d9391b879dfccb
wn.lin <wn.lin@104.com.tw>
2025-12-19 00:02:37 +0800

    update

--- scripts.js
+++ scripts.js
@@ -10,19 +10,6 @@
   f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-NQQQJPDZ');
 
-// Google Analytics (gtag)
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-(function loadGtag() {
-  var g = document.createElement('script');
-  g.async = true;
-  g.src = 'https://www.googletagmanager.com/gtag/js?id=G-GVHQ00L7T7';
-  var f = document.getElementsByTagName('script')[0];
-  f.parentNode.insertBefore(g, f);
-})();
-gtag('js', new Date());
-gtag('config', 'G-GVHQ00L7T7');
-
 // Projects show more/less toggle
 document.addEventListener('DOMContentLoaded', () => {
   const toggle = document.getElementById('projects-toggle');
